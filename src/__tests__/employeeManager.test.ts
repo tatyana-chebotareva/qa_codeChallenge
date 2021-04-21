9fec51c7d9d3c79afee3b473dd63dcf4ca3b42cc
tatyana-chebotareva <myownuk@gmail.com>
2021-04-21 14:41:58 -0400

    #3 Create tests for Employee Manager

--- src/__tests__/employeeManager.test.ts
+++ src/__tests__/employeeManager.test.ts
@@ -1,5 +1,7 @@
 import { EmployeeHandler } from "./pageObjects/EmployeeHandler";
-
+//jest.setTimeout(10000); //may need to set another timeout
+//usually the first test "can add a new employee" takes 3000+ms to run on my PC, 
+//but sometimes it's 7000+ms and it ends up with error "Async callback was not invoked within the 5000 ms timeout"
 const em = new EmployeeHandler();
 
 describe("Employee Manager", () => {
@@ -9,6 +11,7 @@ describe("Employee Manager", () => {
   afterAll(async () => {
     await em.quit();
   });
+
   it("can add a new employee", async () => {
     await em.addEmployee();
     await em.selectEmployeeByName("New Employee");
@@ -25,6 +28,7 @@ describe("Employee Manager", () => {
     expect(employee.phone).toEqual("1234567890");
     expect(employee.title).toEqual("test result");
   });
+
   it("can edit an existing employee", async () => {
     await em.selectEmployeeByName("Bernice Ortiz");
     await em.editEmployee({ title: "Grand Poobah" });
@@ -39,4 +43,38 @@ describe("Employee Manager", () => {
       title: "Grand Poobah",
     });
   });
+
+  test("Test adding one more employee", async () => {
+    await em.addEmployee(); //adds new employee with default info
+    await em.selectEmployeeByName("New Employee");
+    await em.editEmployee({name: "Brian Dala", phone: "8583004525", title: "Meteorologist"}); //changes new employee's info
+    await em.saveChanges();
+    await em.selectEmployeeByName("Dollie Berry");
+    await em.selectEmployeeByName("Brian Dala"); //getting back to added employee to check his info
+    let employee = await em.getEmployeeInfo();
+    expect(employee.name).toEqual("Brian Dala");
+    expect(employee.phone).toEqual("8583004525");
+    expect(employee.title).toEqual("Meteorologist");
+  })
+
+  test("Test cancelling an edit of an employee", async () => {
+    await em.selectEmployeeByName("Bernice Ortiz");
+    await em.editEmployee({name: "Brian Dala", phone: "8583004525", title: "Meteorologist"});  //editing employee's info
+    await em.cancelChanges(); //cancelling changes
+    let employee = await em.getEmployeeInfo(); //checking that changes went away
+    expect(employee).toEqual({id: 1, name: "Bernice Ortiz", phone: "4824931093", title: "CEO"}); // when cancelled it should stay the same.
+    await em.selectEmployeeByName("Phillip Weaver");
+    await em.selectEmployeeByName("Bernice Ortiz"); //getting back to employee to check there are no changes
+    employee = await em.getEmployeeInfo();
+    expect(employee).toEqual({id: 1, name: "Bernice Ortiz", phone: "4824931093", title: "CEO"});
+  })
+
+  test("Test that when editing and then navigating away without saving does not save changes", async () => {
+    await em.selectEmployeeByName("Bernice Ortiz");
+    await em.editEmployee({name: "Brian Dala", phone: "8583004525", title: "Meteorologist",}); //editing employee's info
+    await em.selectEmployeeByName("Phillip Weaver");
+    await em.selectEmployeeByName("Bernice Ortiz"); //getting back to employee to check there are no changes
+    let employee = await em.getEmployeeInfo();
+    expect(employee).toEqual({id: 1, name: "Bernice Ortiz", phone: "4824931093", title: "CEO"});
+  })
 });
