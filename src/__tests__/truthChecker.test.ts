121db3aa3e1d458d8388814dc1b12b961d9b25c3
tatyana-chebotareva <myownuk@gmail.com>
2021-04-20 21:57:04 -0400

    #2 Create data sets to pass each test

--- src/__tests__/truthChecker.test.ts
+++ src/__tests__/truthChecker.test.ts
@@ -1,30 +1,30 @@
 describe("checking for truth", () => {
   test("are the littleNumbers < bigNumbers?", () => {
-    // here are the two arrays to populate in this test.
-    // all littleNumbers entries should be smaller than any bigNumbers entry
-    // both arrays should have the same length.
-    let littleNumbers: Array<number> = [];
-    let bigNumbers: Array<number> = [];
+    let littleNumbers: Array<number> = [-3,1,15];
+    let bigNumbers: Array<number> = [70,99,5054];
 
     for (let i = 0; i < littleNumbers.length && i < bigNumbers.length; i++) {
       expect(littleNumbers[i]).toBeLessThan(bigNumbers[i]);
     }
   });
+
   test("wordToCheck is a palindrome, case and everything", () => {
-    let wordToCheck = "replace me"; // yeah, replace the value to pass the test.
+    let wordToCheck = "racecar";
     expect(wordToCheck.split("").reverse().join("")).toBe(wordToCheck);
   });
+
   test("true", () => {
-    let val = null; //set this to a passing value
+    let val = true;
     expect(val).toBeTruthy();
   });
+
   test("there is a sequence to this test", () => {
-    let values: Array<any> = []; // set values here - the array will take any type or combo of types.
+    let values: Array<any> = ["hi", true, 27, null];
     for (let i = 0; i < values.length; i++) {
-      if (i == 0) expect(typeof values[i]).toBe("string");
-      else if (i == 1) expect(typeof values[i]).toBe("boolean");
-      else if (i == 2) expect(typeof values[i]).toBe("number");
-      else if (i == 3) expect(values[i]).toBeNull();
+      if (i == 0) expect(typeof values[i]).toBe("string"); //values[0] should be String type
+      else if (i == 1) expect(typeof values[i]).toBe("boolean"); //values[1] should be boolean type
+      else if (i == 2) expect(typeof values[i]).toBe("number"); //values[2] should be number type
+      else if (i == 3) expect(values[i]).toBeNull(); //values[3] should be null
       else expect(false).toBe(true);
     }
   });
